f4d7f75749a2d4195315f8dc89270feac3716a88
unknown <jrubi89@icloud.com>
2020-06-27 02:27:42 -0400

    adding require dependency

--- liri.js
+++ liri.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 var fs = require("fs");
 var moment = require("moment");
-
+var request = require("request");
 var axios = require("axios");
 var Spotify = require("node-spotify-api");
 
